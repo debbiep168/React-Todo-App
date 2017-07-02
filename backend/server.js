056367537dc3c46acf5122382280b2498f187b20
DebbiePao <dpao@berkeley.edu>
2017-07-02 08:00:45 -0700

    connected mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dbRoutes from './routes/databaseAccess.js'; //importing all routes from this file
 import mongoose from 'mongoose';
 const app = express();
-var connect = process.env.MONGODB_URI || require('./connect');
+var connect = process.env.MONGODB_URI;
 mongoose.connect(connect);
 
 // This line makes the build folder publicly available.
